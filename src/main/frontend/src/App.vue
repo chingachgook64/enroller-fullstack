d710eb320105407adbd2c1f1ac30b288f1e63207
Dariusz <chingachgook64@gmail.com>
2021-06-12 12:20:32 +0200

    Lab 3 work on exercies

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,52 +1,97 @@
 <template>
   <div id="app">
     <h1>
-      <img src="./assets/logo.svg" alt="Enroller" class="logo">
+      <img src="./assets/logo.svg" alt="Enroller" class="logo" />
       System do zapisów na zajęcia
     </h1>
     <div v-if="authenticatedUsername">
-      <h2>Witaj {{ authenticatedUsername }}!
-        <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
+      <h2>
+        Witaj {{ authenticatedUsername }}!
+        <a @click="logout()" class="float-right button-outline button"
+          >Wyloguj</a
+        >
       </h2>
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+      <button
+        :class="registering ? 'button-outline' : ''"
+        @click="registering = false"
+      >
+        Loguję się
+      </button>
+      <button
+        :class="!registering ? 'button-outline' : ''"
+        @click="registering = true"
+      >
+        Rejestruję się
+      </button>
+      <div v-if="errorMessage" class="alert-warning">
+        {{ errorMessage }}
+      </div>
+      <login-form v-if="!registering" @login="login($event)"></login-form>
+      <login-form
+        v-if="registering"
+        @login="register($event)"
+        button-label="Zarejestruj się"
+      ></login-form>
     </div>
   </div>
 </template>
 
 <script>
-    import "milligram";
-    import LoginForm from "./LoginForm";
-    import MeetingsPage from "./meetings/MeetingsPage";
+import "milligram";
+import LoginForm from "./LoginForm";
+import MeetingsPage from "./meetings/MeetingsPage";
 
-    export default {
-        components: {LoginForm, MeetingsPage},
-        data() {
-            return {
-                authenticatedUsername: ""
-            };
-        },
-        methods: {
-            login(user) {
-                this.authenticatedUsername = user.login;
-            },
-            logout() {
-                this.authenticatedUsername = '';
-            }
-        }
+export default {
+  components: { LoginForm, MeetingsPage },
+  data() {
+    return {
+      authenticatedUsername: "",
+      registering: false,
+      errorMessage: "",
     };
+  },
+  methods: {
+    login(user) {
+      this.authenticatedUsername = user.login;
+    },
+    register(user) {
+      this.$http
+        .post("participants", user)
+        .then((response) => {
+          this.errorMessage = "";
+          this.registering = false;
+        })
+        .catch((response) => {
+          // console.log(response);
+          this.errorMessage = response.bodyText;
+        });
+    },
+    logout() {
+      this.authenticatedUsername = "";
+    },
+  },
+};
 </script>
 
 <style>
-  #app {
-    max-width: 1000px;
-    margin: 0 auto;
-  }
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 
-  .logo {
-    vertical-align: middle;
-  }
+.logo {
+  vertical-align: middle;
+}
+
+.alert-warning {
+  border: 3px red dotted;
+  padding: 5px;
+  background: pink;
+  border-radius: 50%;
+  text-align: center;
+}
 </style>
 
