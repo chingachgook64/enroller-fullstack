86a7fc22cdca3898c64b8e27f5e08985e9bc5bb3
Dariusz <chingachgook64@gmail.com>
2021-06-19 01:43:21 +0200

    Adding meetings to db

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,11 +1,14 @@
 package com.company.enroller.controllers;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +31,114 @@ public class MeetingRestController {
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
     }
+    
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+	    Meeting meeting = meetingService.findById(id);
+	if (meeting == null) { 
+	return new ResponseEntity(HttpStatus.NOT_FOUND);
+	} 
+
+	return new ResponseEntity<Meeting>(meeting, HttpStatus.OK); 
+	}
+	
+	
+	@RequestMapping(value = "/{id}/participants", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeetingParticipants(@PathVariable("id") long id) {
+	    Meeting meeting = meetingService.findById(id);
+	if (meeting == null) { 
+	return new ResponseEntity(HttpStatus.NOT_FOUND);
+	} 
+	
+	Collection<Participant> participants = meeting.getParticipants();
+	return new ResponseEntity<Collection>(participants, HttpStatus.OK); 
+	}
+	
+	
+	@RequestMapping(value = "", method = RequestMethod.POST) 
+	public ResponseEntity<?> registerMeeting(@RequestBody Meeting meeting) {
+		
+		if(meetingService.findById(meeting.getId())!=null ) {
+			
+			return new ResponseEntity("Unable to create. A meeting with id " + meeting.getId() + " already exist.", HttpStatus.CONFLICT);
+		}
+		
+		meetingService.add(meeting);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
+		
+	}
+	
+	@RequestMapping(value = "/{id}/{login}", method = RequestMethod.PUT) 
+	public ResponseEntity<?> addParticipantToMeeting(@PathVariable("id") long id, @PathVariable("login") String login) {
+		
+		ParticipantService participantService = new ParticipantService();
+		Meeting meeting = meetingService.findById(id);
+		
+		if(meeting == null) {
+			
+			return new ResponseEntity("Unable to add participant to meeting . A meeting with id " + id + " does not exist.", HttpStatus.NOT_FOUND);
+		}
+		
+		Participant participant = participantService.findByLogin(login);
+		
+		if(participant == null) {
+			
+			return new ResponseEntity("Unable to add participant to meeting . A participant  with login " + login + " does not exist.", HttpStatus.NOT_FOUND);
+		}
+		
+		meeting.addParticipant(participant);
+		meetingService.updateMeeting(meeting);
+		
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+		
+	}
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT) 
+	public ResponseEntity<?> updateMeeting(@RequestBody Meeting meeting) {
+		
+		if(meetingService.findById(meeting.getId()) == null ) {
+			
+			return new ResponseEntity("Unable to update. A meeting with id " + meeting.getId() + " does not exist.", HttpStatus.NOT_FOUND);
+		}
+		
+		meetingService.updateMeeting(meeting);
+		return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+		
+	}
+	
+	@RequestMapping(value = "/{id}/{login}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipantFromMeeting(@PathVariable("id") long id, @PathVariable("login") String login) {
+	    Meeting meeting = meetingService.findById(id);
+	
+	if (meeting == null) { 
+	return new ResponseEntity(HttpStatus.NOT_FOUND);
+	}
+	
+	ParticipantService participantService = new ParticipantService();
+	Participant participant = participantService.findByLogin(login);
+	
+	if (participant == null) { 
+		return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+
+	meeting.removeParticipant(participant);
+	meetingService.updateMeeting(meeting);
+	
+	return new ResponseEntity<Meeting>(HttpStatus.NO_CONTENT); 
+	
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteMeeting(@PathVariable("id") long id) {
+	    Meeting meeting = meetingService.findById(id);
+	
+	if (meeting == null) { 
+	return new ResponseEntity(HttpStatus.NOT_FOUND);
+	} 
+
+	meetingService.delete(meeting);
+	return new ResponseEntity<Meeting>(HttpStatus.NO_CONTENT); 
+	}
+	
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
 import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -10,9 +12,11 @@ import java.util.Collection;
 public class MeetingService {
 
     DatabaseConnector connector;
+    Session session;
 
     public MeetingService() {
         connector = DatabaseConnector.getInstance();
+        session = connector.getSession();
     }
 
     public Collection<Meeting> getAll() {
@@ -20,6 +24,36 @@ public class MeetingService {
         Query query = connector.getSession().createQuery(hql);
         return query.list();
     }
+    
+    public Meeting findById(long id) {
+		
+		Meeting meeting = (Meeting)this.session.get(Meeting.class, id);
+		
+		return  meeting;
+		
+	}
+	
+	public void add(Meeting meeting) {
+		
+		Transaction transaction = this.session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+	}
+	
+	public void delete(Meeting meeting) {
+		
+		Transaction transaction = this.session.beginTransaction();
+		session.delete(meeting);
+		transaction.commit();
+	}
+	
+	public void updateMeeting(Meeting meeting) {
+		
+		Transaction transaction = this.session.beginTransaction();
+		session.clear();
+		session.update(meeting);
+		transaction.commit();
+	}
 
 
 }
